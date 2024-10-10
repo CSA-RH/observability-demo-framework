88f521d394919f4d1ec5cdc4d21c5208d9fe024e
bizcochillo <apalma@nomail.com>
2024-10-10 16:41:08 +0200

    Replace localhost

--- obs-front/src/ApiHelper.js
+++ obs-front/src/ApiHelper.js
@@ -1,5 +1,4 @@
-//export const MASTER_API_ADDRESS = 'http://localhost:8000'
-export const MASTER_API_ADDRESS = 'https://obs-main-api-observability-demo.apps.zm1iwcvbvd702c1f99.germanywestcentral.aroapp.io'
+export const MASTER_API_ADDRESS = 'http://localhost:8000'
 
 export function getInfoUrl()  {
     return `${MASTER_API_ADDRESS}/info`
