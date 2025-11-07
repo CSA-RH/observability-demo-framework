241f8e7b66b953eaa8f2a0b3eb9d87aee477befb
abel <abel@abel.com>
2025-11-07 10:37:37 +0100

    Change layers per waves folder name

--- install/bash/clean-ns.sh
+++ install/bash/clean-ns.sh
@@ -1,4 +1,4 @@
-NAMESPACES_TO_DELETE=("openshift-logging" "openshift-loki-operator" "openshift-tempo-operator" "openshift-opentelemetry-operator", "minio")
+NAMESPACES_TO_DELETE=("openshift-logging" "openshift-loki-operator" "openshift-tempo-operator" "openshift-opentelemetry-operator", " obs-demo")
 
 for ns in ${NAMESPACES_TO_DELETE[@]}; do
     echo Deleting namespace $ns...
