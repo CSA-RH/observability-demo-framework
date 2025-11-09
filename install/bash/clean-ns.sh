3833dae8a7c22b04ef8d0884698c3b7a5109ff8e
abel <abel@abel.com>
2025-11-09 06:14:30 +0100

    Fix kustomization for ARO and ROSA

--- install/bash/clean-ns.sh
+++ install/bash/clean-ns.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+source ./env.sh
+
 NAMESPACES_TO_DELETE=("openshift-logging" "openshift-loki-operator" "openshift-tempo-operator" "openshift-opentelemetry-operator", " obs-demo")
 
 for ns in ${NAMESPACES_TO_DELETE[@]}; do
