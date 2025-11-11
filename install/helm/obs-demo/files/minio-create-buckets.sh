166536d1a22c390db0abcdae7b2506119ee626db
abel <abel@abel.com>
2025-11-11 23:48:07 +0100

    One Helm to rule them all

---
+++ install/helm/obs-demo/files/minio-create-buckets.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+# Define a writeable config directory inside the container
+CONFIG_DIR="/tmp/.mc"
+
+# Use the --config-dir flag on ALL mc commands
+echo "Using mc config directory: $CONFIG_DIR"
+mc --config-dir $CONFIG_DIR --version
+
+echo "Setting alias..."
+mc --config-dir $CONFIG_DIR alias set obs-minio http://minio.${PROJECT_NAME}.svc:9000 minioadmin minioadmin123
+
+echo "Listing buckets..."
+mc --config-dir $CONFIG_DIR ls obs-minio
+
+# Use --ignore-existing to make the script idempotent
+echo "Creating buckets..."
+mc --config-dir $CONFIG_DIR mb --ignore-existing obs-minio/loki-storage
+mc --config-dir $CONFIG_DIR mb --ignore-existing obs-minio/tempo-storage
+
+echo "✅ Minio buckets are ready."
