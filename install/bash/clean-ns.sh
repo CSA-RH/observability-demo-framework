77b08f0cb312baae5ccb978a764f42731be62f3a
abel <abel@abel.com>
2025-11-12 14:46:57 +0100

    OTel wave fix

--- install/bash/clean-ns.sh
+++ install/bash/clean-ns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./env.sh
 
-NAMESPACES_TO_DELETE=("openshift-logging" "openshift-loki-operator" "openshift-tempo-operator" "openshift-opentelemetry-operator", " obs-demo")
+NAMESPACES_TO_DELETE=("openshift-logging" "openshift-loki-operator" "openshift-tempo-operator" "openshift-opentelemetry-operator" "obs-demo")
 
 for ns in ${NAMESPACES_TO_DELETE[@]}; do
     echo Deleting namespace $ns...
