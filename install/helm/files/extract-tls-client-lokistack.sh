8dc5317baf5bbdb2169188c09a4896cdc8980668
abel <abel@abel.com>
2025-11-15 10:53:15 +0100

    Configure Loki OTLP

---
+++ install/helm/files/extract-tls-client-lokistack.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -eu
+
+# Export the LokiStack logging service CA into the target namespace
+export SERVICE_CA=$(oc get configmap logging-ca-bundle \
+                       -n openshift-logging \
+                       -ojsonpath='{.data.service-ca\.crt}')
+oc create configmap logging-ca-bundle \
+   -n $NAMESPACE \
+   --from-literal=service-ca\.crt=$SERVICE_CA \
+   -oyaml \
+   --dry-run=client \
+   | oc apply -f -
+echo CONFIGMAP updated
+
+# Export the LokiStack Gateway Client TLS certificate into the target namespace
+export CLIENT_TLS_CRT_FILE=/tmp/logging-client-tls.crt
+export CLIENT_TLS_KEY_FILE=/tmp/logging-client-tls.key
+oc get secret logging-gateway-client-http \
+   -n openshift-logging \
+   -ojsonpath='{.data.tls\.crt}' | base64 -d \
+   > $CLIENT_TLS_CRT_FILE
+echo CLIENT CERTIFICATE DEFINITION retrieved
+oc get secret logging-gateway-client-http \
+   -n openshift-logging \
+   -ojsonpath='{.data.tls\.key}' | base64 -d \
+   > $CLIENT_TLS_KEY_FILE
+echo CLIENT CERTIFICATE KEY retrieved
+oc create secret tls logging-gateway-client-http \
+   -n $NAMESPACE \
+   --cert=$CLIENT_TLS_CRT_FILE \
+   --key=$CLIENT_TLS_KEY_FILE \
+   -oyaml \
+   --dry-run=client \
+   | oc apply -f -
+echo CLIENT CERTIFICATE DEFINITION updated
